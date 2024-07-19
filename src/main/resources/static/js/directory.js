a0b7781d7f3e2b2280b1185e4b1d018add6eaf45
Lamool <gosu0522@naver.com>
2024-07-19 18:01:33 +0900

    0719 수정

---
+++ src/main/resources/static/js/directory.js
@@ -0,0 +1,59 @@
+
+// 부서등록
+function dAdd(){
+    let name = document.querySelector("#name").value
+    let contact = document.querySelector('#contact').value
+
+    let info = {name : name , contact , contact};
+
+    $.ajax( {
+        async : false ,         
+        method : 'post' ,      
+        url : "/directory/add", 
+        data : info ,           
+        success : ( result )=>{ console.log( result ); 
+            
+            if( result ){alert('부서등록 성공');
+                location.href="/directory/index";
+            }else{  alert('부서등록 실패');  }
+        } // success end
+    } ); // ajax end
+}
+
+dPrint();
+// 전체 출력
+function dPrint(){
+    $ajax({
+        async : false ,         
+        method : 'get' ,      
+        url : "/directory/dprint",            
+        success : (result)=>{
+            let printBox = document.querySelector('#printBox')
+            html = '';
+            for (dto of result){
+                html += `   <tr>
+                                <th scope="row">${dto.dno}</th>
+                                <td>${dto.dname}</td>
+                                <td>${dto.dcontact}</td>
+                            </tr>
+                           `
+            }
+            printBox.innerHTML = html;
+        }
+    });
+}
+
+
+// 부서수정
+
+
+
+
+
+// 부서삭제
+
+
+
+
+
+
